aecc5ce6e923b17702f950de2b25cddbcdf4d8f0
Gaoyifan66 <2498712128@qq.com>
2020-04-17 16:21:26 +0800

    网络数据的获取

--- app/src/main/java/com/swufe/firstapp/RateActivity.java
+++ app/src/main/java/com/swufe/firstapp/RateActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,7 +18,15 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class RateActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class RateActivity extends AppCompatActivity implements Runnable{
 
     private final String TAG = "Rate";
     private float dollarRate = 0.1f;
@@ -26,6 +36,7 @@ public class RateActivity extends AppCompatActivity {
 
     EditText rmb;
     TextView show;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +54,22 @@ public class RateActivity extends AppCompatActivity {
         Log.i(TAG,"onCreate: sp dollarRate=" + dollarRate);
         Log.i(TAG,"onCreate: sp euroRate=" + euroRate);
         Log.i(TAG,"onCreate: sp wonRate=" + wonRate);
+
+        //开启子线程
+        Thread t = new Thread(this);
+        t.start();
+
+        handler = new Handler(){
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                if(msg.what==5){
+                    String str = (String) msg.obj;
+                    Log.i(TAG, "handleMessage: getMessage msg = "+str);
+                    show.setText(str);
+                }
+                super.handleMessage(msg);
+            }
+        };
     }
 
     public void onClick(View btn){
@@ -68,10 +95,13 @@ public class RateActivity extends AppCompatActivity {
         }
 
     }
+
+
     public void openOne(View btn){
         //打开一个页面activity
         openConfig();
     }
+
 
     private void openConfig() {
         Intent config = new Intent(this, ConfigActivity.class);
@@ -126,5 +156,55 @@ public class RateActivity extends AppCompatActivity {
 
         }
         super.onActivityResult(requestCode,resultCode,data);
+    }
+
+    public void run(){
+        Log.i(TAG,"run:run()......");
+        for(int i=1;i<3;i++) {
+            Log.i(TAG,"run:i=" + i);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        //获取Msg对象，用于返回主线程
+        Message msg = handler.obtainMessage(5);
+        //msg.what = 5;
+        msg.obj = "Hello from run()";
+        handler.sendMessage(msg);
+
+
+        //获取网络数据
+        URL url = null;
+        try {
+            url = new URL("https://www.usd-cny.com/icbc.htm");
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            InputStream in = http.getInputStream();
+
+            String html = inputStream2String(in);
+            Log.i(TAG, "run: html=" + html);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+    //输入流转换成字符串输出
+    private String inputStream2String(InputStream inputStream) throws IOException {
+        final int bufferSize = 1024;
+        final char[] buffer = new char[bufferSize];
+        final StringBuilder out = new StringBuilder();
+        Reader in = new InputStreamReader(inputStream, "gb2312");
+        for(; ;){
+            int rsz = in.read(buffer,0,buffer.length);
+            if(rsz<0)
+               break;
+            out.append(buffer, 0, rsz);
+        }
+        return out.toString();
     }
 }
