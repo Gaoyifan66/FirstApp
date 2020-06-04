efae060dbcaf88863d580523604c30c2ebb3ba3e
Gaoyifan66 <2498712128@qq.com>
2020-06-04 19:25:34 +0800

    SQLite数据库的使用push

---
+++ app/src/main/java/com/swufe/firstapp/RateItem.java
@@ -0,0 +1,45 @@
+package com.swufe.firstapp;
+
+public class RateItem {
+
+    private int id;
+    private String curName;
+    private String curRate;
+
+    public RateItem() {//空构造函数
+        super();
+        curName = "";
+        curRate = "";
+    }
+
+    public RateItem(String curName, String curRate) {
+        this.curName = curName;
+        this.curRate = curRate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCurName() {
+        return curName;
+    }
+
+    public void setCurName(String curName) {
+        this.curName = curName;
+    }
+
+    public String getCurRate() {
+        return curRate;
+    }
+
+    public void setCurRate(String curRate) {
+        this.curRate = curRate;
+    }
+
+
+}
